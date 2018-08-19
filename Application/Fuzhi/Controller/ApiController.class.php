5825ab67b3ff2510eafd79d848ea19e78ba5ee08
szw <992950293@qq.com>
2018-08-19 21:23:14 +0800

    修改 彩种

---
+++ Application/Fuzhi/Controller/ApiController.class.php
@@ -0,0 +1,55 @@
+<?php
+namespace Fuzhi\Controller;
+
+use Think\Controller;
+class ApiController extends Controller{
+    public function index(){
+       // echo 1111;exit;
+        if(empty($_POST)){
+            $arr = array(
+                'code'=>false,
+                'msg'=>'参数为空',
+            );
+            return json_encode($arr,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        }
+        file_put_contents('lgc.log',date("Y-m-d H:i:s".$_POST),FILE_APPEND);
+        $data = json_decode($_POST,true);
+        if(empty($data['name'])){
+            $arr = array(
+                'code'=>false,
+                'msg'=>'彩种为空',
+            );
+            return json_encode($arr,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        }
+        $res = M('lot_type')->where(array('name'=>$data['name']))->find();
+        file_put_contents('lgc.log',date("Y-m-d H:i:s".$res),FILE_APPEND);
+        if(empty($res)){
+            $arr = array(
+                'code'=>false,
+                'msg'=>'没有这个彩种',
+            );
+            return json_encode($arr,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        }
+        $where = array(
+            'dat_type'=>$res['id'],
+            'dat_open_time'=>time(),
+            'dat_expect'=>$data['issue'],
+            'dat_codes'=>$data['code'],
+        );
+        $res = M('lot_data')->add($where);
+        file_put_contents('lgc.log',date("Y-m-d H:i:s".$res),FILE_APPEND);
+        if($res < 1){
+            $arr = array(
+                'code'=>false,
+                'msg'=>'系统有误',
+            );
+            return json_encode($arr,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        }
+
+        $arr = array(
+            'code'=>true,
+            'msg'=>'成功',
+        );
+        return json_encode($arr,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    }
+}
--- js/indexlotaward.js
+++ js/indexlotaward.js
@@ -492,6 +492,55 @@ $(function () {
     };
 
 
+
+    var gd11x5_awardTick = function () {
+        $.get('gd11x5/getgd11x5AwardData.do?' + Math.random(), { ajaxhandler: 'GetPk10AwardData' }, function (data) {
+            if(!data)
+                return;
+            if ((data.current.periodNumber != pk10_currentPeriodNumber)) {
+
+                $("#gd11x5 .kajianhao").html("");
+                var nums = data.current.awardNumbers.split(',');
+                var str = Number(nums[0])+Number(nums[1])+Number(nums[2])+Number(nums[3])+Number(nums[4]);
+                //  cpNumber = data.current.periodNumber;
+
+                $("#gd11x5 .longhu2").find(".sumNum").text(str),
+                    $("#gd11x5 .longhu2").find(".sumSingleDouble").text(str%2 == 0 ? "双" : "单"),
+                    $("#gd11x5 .longhu2").find(".sumBigSmall").text(str == 30? "和":str > 30 ? "大" : "小");
+
+                for (var i = 0; i < nums.length; i++) {
+                        $("#gd11x5 .kajianhao").append("<li class='numblueHead'>"+nums[i]+"</li>")
+                }
+
+
+                if ((nums[0] == nums[1]) && (nums[0] == nums[2]))
+                {    type = '豹子';}
+                else if (((nums[1] - nums[0]) == (nums[2] - nums[1])) && ((nums[1] - nums[0]) == 1) || (nums == Array(0, 8, 9) || nums == Array(0, 1, 9))) {    type = '顺子';}
+                else if (nums[0] == nums[1] || nums[1] == nums[2]) {    type = '对子';} else if ((nums[1] - nums[0]) == 1 || (nums[2] - nums[1]) == 1) {    type = '半顺';}
+                else {    type = '杂六';}$("#tjSsc").find(".behindThree").text(type);var type1 = '';if ((nums[1] == nums[2]) && (nums[1] == nums[3])) {    type1 = '豹子';}
+                else if (((nums[2] - nums[1]) == (nums[3] - nums[2])) && ((nums[2] - nums[1]) == 1) || (nums == Array(0, 8, 9) || nums == Array(0, 1, 9))) {    type1 = '顺子';}
+                else if (nums[1] == nums[2] || nums[2] == nums[3]) {    type1 = '对子';} else if ((nums[2] - nums[1]) == 1 || (nums[3] - nums[2]) == 1) {    type1= '半顺';}
+                else {    type1 = '杂六';}$("#tjSsc").find(".betweenThree").text(type1);var type2 = '';if ((nums[2] == nums[3]) && (nums[2] == nums[4]))
+                {    type2 = '豹子';} else if (((nums[3] - nums[2]) == (nums[4] - nums[3])) && ((nums[3] - nums[2]) == 1) || (nums == Array(0, 8, 9) || nums == Array(0, 1, 9)))
+                {    type2 = '顺子';} else if (nums[2] == nums[3] || nums[3] == nums[4]) {    type2 = '对子';} else if ((nums[3] - nums[2]) == 1 || (nums[4] - nums[3]) == 1)
+                {    type2= '半顺';} else {    type2 = '杂六';}
+
+
+
+                cpNumber = data.current.periodNumber;
+                var _time = data.current.awardTime.substring(11, 16);
+                //  $(".num_ul .preDrawIssue").html(data.current.periodNumber);
+                $("#cqSsc_hot .drawCount").html(data.next.periodNumber);
+                $("#cqSsc_hot .sdrawCountnext").html(data.firstPeriod+179-cpNumber);
+
+                //drawTrend();
+            }
+            pk10_currentPeriodNumber = data.current.periodNumber;
+            window.setTimeout(gd11x5_awardTick,data.next.awardTimeInterval < 10 ? 10000 : data.next.awardTimeInterval);
+        }, 'json');
+    };
+
+
     function typeOf(e, t) {
         if ("san" == e) switch (1 * t) {
             case 0:
@@ -535,6 +584,7 @@ $(function () {
     window.setTimeout(tjssc_awardTick, 5000);
     window.setTimeout(jsssc_awardTick, 5000);
     window.setTimeout(jssc_awardTick, 5000);
+    window.setTimeout(gd11x5_awardTick, 5000);
 //    window.setTimeout(gd_awardTick, 500);
 
     $(".award .lot-menu").hover(function () {
