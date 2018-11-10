6cbf73d9778a8f068a7ba6fc0684baa0cb9c4bb9
zhoujielun7777 <zhoujielun77777@gmail.com>
2018-11-10 17:27:09 +0800

    20181103

--- m/pk10/index.php
+++ m/pk10/index.php
@@ -130,7 +130,7 @@ include("../conn.php");
 <!--    </div>-->
 <!--</div>-->
 <div class="choose" id="chooseNum" style="display: none">
-    <div class="chooseRow1">
+    <div class="chooseRow1 chooseRow1-on">
         <a>1</a>
         <a>2</a>
         <a>3</a>
